de55c44ee1113a8a90d598b606130f01fe78777f
Shivam Yadav <117904778+shivamyadavskn@users.noreply.github.com>
2023-10-19 00:44:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-//complete this code
-class Rectangle {}
+class Rectangle {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
-class Square extends Animal {}
+  get width() {
+    return this._width;
+  }
+
+  get height() {
+    return this._height;
+  }
+
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side); // Call the parent class constructor with the same side for width and height
+  }
+
+  getPerimeter() {
+    return 4 * this._width; // Perimeter of a square is 4 times its side
+  }
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
